b859a9c18b6a34b9cdc8472138acd36049925f87
Alejandra Izquierdo <allleizq@gmail.com>
2022-06-23 12:02:27 -0700

    associate computer random numbers with letters

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var computerChoice;
 
 // have the computer choose R, P, S at random
 computerChoice = (Math.floor(Math.random() * 3));
+function choose(){
+    randomnumber = (Math.floor(Math.random() * 3));
+    if (randomnumber == 0) {
+        console.log("r")
+    }
+}
 
 // prompt user to enter their choice of rock, paper, or scissors
 userChoice = prompt("Let's play a game! Please enter your choice of R, P, or S:");
