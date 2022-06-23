65ee6ec6396a263c884d072416b3a3b43384ae11
lshillman <lshillman@users.noreply.github.com>
2022-06-23 12:30:54 -0700

    set computerChoice variable equal to the computer's random choice. Add some win conditions.

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ var userChoice;
 var computerChoice;
 
 // have the computer choose R, P, S at random
-computerChoice = (Math.floor(Math.random() * 3));
 function choose(){
     randomnumber = (Math.floor(Math.random() * 3));
     if (randomnumber == 0) {
-        console.log("r");
+        computerChoice = "R";
     }
     else if(randomnumber == 1) {
-        console.log("p");
+        computerChoice = "P";
     }
     else if(randomnumber == 2) {
-        console.log("s");
+        computerChoice = "S";
     }
+    console.log("Computer chooses " + computerChoice + "!");
 }
+
+choose();
 
 // prompt user to enter their choice of rock, paper, or scissors
 userChoice = prompt("Let's play a game! Please enter your choice of R, P, or S:");
+
+if (userChoice == computerChoice) {
+    alert("It's a tie!");
+} else if ((userChoice == R) && (computerChoice == P)) {
+    alert("The computer wins");
+}
 
 // do something to see whether the user won, lost, or tied with the computer
